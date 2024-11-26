41b77ab901b307a47bab175c4ecdea021f69c48a
Your Name <you@example.com>
2024-11-26 10:37:36 +0000

    ccc

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  
+autoplay:{
+  delay:800,
+  disableOnInteraction: false,
+},
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
     
-    autoplay:{
-        delay: 800,
-        disableOnInteraction: false,
-    },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  });
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
